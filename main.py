f1891572ae5fd1bd362482cf9e8f510d588dfb98
SidewaysPhoenix <madams2550@gmail.com>
2025-09-02 14:38:10 -0500

    asteroid field included

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -8,22 +10,38 @@ def main():
     clock = pygame.time.Clock() #object to keep track of time
     dt = 0
 
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+
+        for drawables in drawable:
+            drawables.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
         
-
-
 
 if __name__ == "__main__":
     main()
